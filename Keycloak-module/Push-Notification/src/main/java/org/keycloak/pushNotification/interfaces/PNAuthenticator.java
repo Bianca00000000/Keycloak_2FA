a079c3fc20c257c1cc26a53e9b5d1a062347f8d5
Bianca Ionascu <ionascubianca17@yahoo.com>
2024-02-29 17:23:12 +0200

    Completare PN
    Signed-off-by: Bianca Ionascu <ionascubianca17@yahoo.com>

--- Keycloak-module/Push-Notification/src/main/java/org/keycloak/pushNotification/interfaces/PNAuthenticator.java
+++ Keycloak-module/Push-Notification/src/main/java/org/keycloak/pushNotification/interfaces/PNAuthenticator.java
@@ -2,7 +2,22 @@ package org.keycloak.pushNotification.interfaces;
 
 import org.keycloak.authentication.Authenticator;
 import org.keycloak.authentication.CredentialValidator;
+import org.keycloak.http.HttpCookie;
+import org.keycloak.http.HttpResponse;
+import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.authentication.AuthenticationFlowError;
+import org.keycloak.authentication.RequiredActionFactory;
+import org.keycloak.authentication.RequiredActionProvider;
+import org.keycloak.credential.CredentialProvider;
+import org.keycloak.models.AuthenticatorConfigModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserCredentialModel;
+import org.keycloak.models.UserModel;
 
+import jakarta.ws.rs.core.Cookie;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.Response;
 import java.net.URI;
 import java.util.Collections;
 import java.util.List;
@@ -10,81 +25,27 @@ import java.util.List;
 public class PNAuthenticator implements Authenticator, CredentialValidator<PNCredentialProvider> {
 
     @Override
-    public void authenticate(AuthenticationFlowContext context) {
-        if (hasCookie(context)) {
-            context.success();
-            return;
-        }
-        Response challenge = context.form()
-                .createForm("secret-question.ftl");
-        context.challenge(challenge);
-    }
-
-    @Override
-    public void action(AuthenticationFlowContext context) {
-        boolean validated = validateAnswer(context);
-        if (!validated) {
-            Response challenge =  context.form()
-                    .setError("badSecret")
-                    .createForm("secret-question.ftl");
-            context.failureChallenge(AuthenticationFlowError.INVALID_CREDENTIALS, challenge);
-            return;
-        }
-        setCookie(context);
-        context.success();
-    }
-
-    protected void setCookie(AuthenticationFlowContext context) {
-        AuthenticatorConfigModel config = context.getAuthenticatorConfig();
-        int maxCookieAge = 60 * 60 * 24 * 30; // 30 days
-        if (config != null) {
-            maxCookieAge = Integer.valueOf(config.getConfig().get("cookie.max.age"));
-
-        }
-        URI uri = context.getUriInfo().getBaseUriBuilder().path("realms").path(context.getRealm().getName()).build();
-        addCookie(context, "SECRET_QUESTION_ANSWERED", "true",
-                uri.getRawPath(),
-                null, null,
-                maxCookieAge,
-                false, true);
-    }
-
-    public void addCookie(AuthenticationFlowContext context, String name, String value, String path, String domain, String comment, int maxAge, boolean secure, boolean httpOnly) {
-        HttpResponse response = context.getSession().getContext().getHttpResponse();
-        response.setCookieIfAbsent(new HttpCookie(1, name, value, path, domain, comment, maxAge, secure, httpOnly, null));
-    }
-
-
-    protected boolean validateAnswer(AuthenticationFlowContext context) {
-        MultivaluedMap<String, String> formData = context.getHttpRequest().getDecodedFormParameters();
-        String secret = formData.getFirst("secret_answer");
-        String credentialId = formData.getFirst("credentialId");
-        if (credentialId == null || credentialId.isEmpty()) {
-            credentialId = getCredentialProvider(context.getSession())
-                    .getDefaultCredential(context.getSession(), context.getRealm(), context.getUser()).getId();
-        }
-
-        UserCredentialModel input = new UserCredentialModel(credentialId, getType(context.getSession()), secret);
-        return getCredentialProvider(context.getSession()).isValid(context.getRealm(), context.getUser(), input);
-    }
-
-    @Override
     public boolean requiresUser() {
         return true;
     }
 
-    @Override
+    /*@Override
     public boolean configuredFor(KeycloakSession session, RealmModel realm, UserModel user) {
         return getCredentialProvider(session).isConfiguredFor(realm, user, getType(session));
-    }
+    }*/
 
     @Override
     public void setRequiredActions(KeycloakSession session, RealmModel realm, UserModel user) {
-        user.addRequiredAction(SecretQuestionRequiredAction.PROVIDER_ID);
+        user.addRequiredAction(PNRequiredAction.PROVIDER_ID);
     }
 
     public List<RequiredActionFactory> getRequiredActions(KeycloakSession session) {
-        return Collections.singletonList((SecretQuestionRequiredActionFactory)session.getKeycloakSessionFactory().getProviderFactory(RequiredActionProvider.class, SecretQuestionRequiredAction.PROVIDER_ID));
+        return Collections.singletonList((PNRequiredActionFactory)session.getKeycloakSessionFactory().getProviderFactory(RequiredActionProvider.class, PNRequiredAction.PROVIDER_ID));
+    }
+
+    @Override
+    public PNCredentialProvider getCredentialProvider(KeycloakSession session) {
+        return (PNCredentialProvider) session.getProvider(CredentialProvider.class, PNCredentialProviderFactory.PROVIDER_ID);
     }
 
     @Override
@@ -92,9 +53,51 @@ public class PNAuthenticator implements Authenticator, CredentialValidator<PNCre
 
     }
 
+    protected boolean hasCookie(AuthenticationFlowContext context) {
+        Cookie cookie = context.getHttpRequest().getHttpHeaders().getCookies().get("PUSH_NOTIFICATION_COMPLETED");
+        boolean result = cookie != null;
+        if (result) {
+            System.out.println("Bypassing push notification because cookie is set");
+        }
+        return result;
+    }
+
+    public void addCookie(AuthenticationFlowContext context, String name, String value, String path, String domain, String comment, int maxAge, boolean secure, boolean httpOnly) {
+        HttpResponse response = context.getSession().getContext().getHttpResponse();
+        response.setCookieIfAbsent(new HttpCookie(1, name, value, path, domain, comment, maxAge, secure, httpOnly, null));
+    }
+
+    protected void setCookie(AuthenticationFlowContext context) {
+        AuthenticatorConfigModel config = context.getAuthenticatorConfig();
+        int maxCookieAge = 60 * 60 * 24 * 30; // 30 days
+        if (config != null) {
+            maxCookieAge = Integer.valueOf(config.getConfig().get("cookie.max.age"));
+        }
+        URI uri = context.getUriInfo().getBaseUriBuilder().path("realms").path(context.getRealm().getName()).build();
+        addCookie(context, "PUSH_NOTIFICATION_COMPLETED", "true",
+                uri.getRawPath(),
+                null, null,
+                maxCookieAge,
+                false, true);
+    }
+
     @Override
-    public SecretQuestionCredentialProvider getCredentialProvider(KeycloakSession session) {
-        return (SecretQuestionCredentialProvider)session.getProvider(CredentialProvider.class, SecretQuestionCredentialProviderFactory.PROVIDER_ID);
+    public void authenticate(AuthenticationFlowContext context) {
+        if (hasCookie(context)) {
+            context.success();
+            return;
+        }
+        // DE IMPLEMENTAT LOGICA PENTRU PARTEA DE AUTENTIFICARE CU PN => AFISARE PAGINA WEB PT A INTRODUCE CODUL
+    }
+
+    @Override
+    public void action(AuthenticationFlowContext context) {
+        // In cazul in care este valid codul setam cookie-ul altfel da o eroare si ii spunem sa reincerce conectarea
+    }
+
+    protected boolean validateAnswer(AuthenticationFlowContext context) {
+        return false;
+        // De verficat codul introdus de utilizator.
     }
 
 }
---
+++ Keycloak-module/Push-Notification/src/main/java/org/keycloak/pushNotification/interfaces/PNRequiredActionFactory.java
@@ -0,0 +1,42 @@
+package org.keycloak.pushNotification.interfaces;
+
+import org.keycloak.Config;
+import org.keycloak.authentication.RequiredActionFactory;
+import org.keycloak.authentication.RequiredActionProvider;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+
+public class PNRequiredActionFactory implements RequiredActionFactory {
+
+    private static final PNRequiredAction SINGLETON = new PNRequiredAction();
+
+    @Override
+    public RequiredActionProvider create(KeycloakSession session) {
+        return SINGLETON;
+    }
+
+    @Override
+    public String getId() {
+        return PNRequiredAction.PROVIDER_ID;
+    }
+
+    @Override
+    public String getDisplayText() {
+        return "Push Notification";
+    }
+
+    @Override
+    public void init(Config.Scope config) {
+
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory factory) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
