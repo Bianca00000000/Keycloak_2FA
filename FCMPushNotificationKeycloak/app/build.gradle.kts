2766657f2d35c9b1c2026f20fd09fcbb8a5eb002
Bianca Ionascu <ionascubianca17@yahoo.com>
2024-03-25 02:27:19 +0200

    Modul PN Android
    Signed-off-by: Bianca Ionascu <ionascubianca17@yahoo.com>

--- FCMPushNotificationKeycloak/app/build.gradle.kts
+++ FCMPushNotificationKeycloak/app/build.gradle.kts
@@ -48,6 +48,9 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    // Style
+    implementation("androidx.cardview:cardview:1.0.0")
+
     // OkHttp logging interceptor
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
--- Keycloak-module/Push-Notification/src/main/java/org/keycloak/pushNotification/interfaces/PNRequiredAction.java
+++ Keycloak-module/Push-Notification/src/main/java/org/keycloak/pushNotification/interfaces/PNRequiredAction.java
@@ -1,7 +1,7 @@
 package org.keycloak.pushNotification.interfaces;
 
-import jakarta.inject.Inject;
-import jakarta.inject.Singleton;
+import com.google.auth.oauth2.AccessToken;
+import com.google.auth.oauth2.GoogleCredentials;
 import jakarta.json.Json;
 import jakarta.json.JsonObject;
 import jakarta.ws.rs.core.Response;
@@ -10,6 +10,7 @@ import org.keycloak.credential.CredentialProvider;
 import org.keycloak.models.UserModel;
 import org.keycloak.pushNotification.model.PNCredentialModel;
 
+import java.io.*;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -18,7 +19,7 @@ import java.util.List;
 import java.util.Random;
 
 public class PNRequiredAction implements RequiredActionProvider, CredentialRegistrator {
-    private final FirebaseInitializer firebaseInitializer = new FirebaseInitializer();
+
     public static final String PROVIDER_ID = "push_notification_config";
 
     @Override
@@ -35,18 +36,20 @@ public class PNRequiredAction implements RequiredActionProvider, CredentialRegis
 
         if (tokensFCM != null) {
             try {
-                firebaseInitializer.initialize();
-                String accessToken = firebaseInitializer.getAccessToken();
                 HttpClient client = HttpClient.newHttpClient();
                 HttpRequest request = HttpRequest.newBuilder()
+                        //.uri(new URI("https://fcm.googleapis.com/v1/projects/fcmpushnotificationkeycloak/messages:send"))
+                        //.header("Content-Type", "application/json; UTF-8")
+                        //.header("Authorization", "Bearer " + getAccessToken())
                         .uri(new URI("https://fcm.googleapis.com/fcm/send"))
                         .header("Content-Type", "application/json")
-                        .header("Authorization", "Bearer " + accessToken)
-                        .POST(HttpRequest.BodyPublishers.ofString(buildMessage(tokenFCM, "Autentificare noua", "Aici este codul tau: " + code)))
+                        .header("Authorization", "Bearer " + "AAAAodfaR5o:APA91bGmGRGhTSNwxZlkke-JrFrI7o5iKG7Va456qDIpwENxlQ0d0Ruvv5XnxkaRAqJR1tPvW5OePMiLYgcnI-cA3653Dun0M4iIvBmXwP870scKEygEI8GQadS5_gGJ54psLOQJp_Ut")
+                        //.POST(HttpRequest.BodyPublishers.ofString(buildV1Message(tokenFCM, "Title", "Code: " + code)))
+                        .POST(HttpRequest.BodyPublishers.ofString(buildMessage(tokenFCM, "Title", "Code: " + code)))
                         .build();
 
                 HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-                System.out.println("Access token: " + accessToken);
+                // System.out.println("Access token: " + getAccessToken());
                 System.out.println("Response status: " + response.statusCode());
                 System.out.println("Response body: " + response.body());
             } catch (Exception e) {
@@ -54,28 +57,42 @@ public class PNRequiredAction implements RequiredActionProvider, CredentialRegis
             }
         }
 
-//        if (tokenFCM != null) {
-//            Message message = Message.builder()
-//                    .setToken(tokenFCM)
-//                    .setNotification(Notification.builder()
-//                            .setTitle("Autentificare noua")
-//                            .setBody("Aici este codul tau: " + code)
-//                            .build())
-//                    .build();
-//
-//            // LOGGER HERE
-//            try {
-//                String response = FirebaseMessaging.getInstance().send(message);
-//                System.out.println("Successfully sent message: " + response);
-//            } catch (Exception e) {
-//                System.err.println("Failed to send message: " + e.getMessage());
-//                return;
-//            }
-//        }
-
         Response challenge = context.form()
                 .createForm("push-notification-form.ftl");
         context.challenge(challenge);
+    }
+
+    private static String getAccessToken() throws IOException {
+        // When loading resources in this way, it's important to close the stream after we're done with it to
+        // avoid memory leaks => I use a try-with-resources (try (...) { ... })
+        // which will automatically closes the InputStream after the try block finishes.
+        try (InputStream serviceAccount = PNRequiredAction.class.getResourceAsStream("/fcm.json")) {
+            if (serviceAccount == null) {
+                throw new FileNotFoundException("Could not find file fcm.json in resources.");
+            }
+
+            GoogleCredentials googleCredentials = GoogleCredentials
+                    .fromStream(serviceAccount)
+                    .createScoped(List.of("https://www.googleapis.com/auth/firebase.messaging"));
+            googleCredentials.refreshIfExpired();
+            AccessToken token = googleCredentials.getAccessToken();
+            return token.getTokenValue();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    private String buildV1Message(String token, String title, String body) {
+        JsonObject message = Json.createObjectBuilder()
+                .add("message", Json.createObjectBuilder()
+                        .add("token", token)
+                        .add("notification", Json.createObjectBuilder()
+                                .add("title", title)
+                                .add("body", body)))
+                .build();
+        return message.toString();
     }
 
     private String buildMessage(String token, String title, String body) {
@@ -87,6 +104,7 @@ public class PNRequiredAction implements RequiredActionProvider, CredentialRegis
                 .build();
         return message.toString();
     }
+
 
     @Override
     public void processAction(RequiredActionContext context) {
